d356a67ce990c21675ddd4a22a5144886339bf26
Md ShahadatKabir <60102939+ShahadatKabir@users.noreply.github.com>
2024-03-07 11:34:19 +0600

    First Commit

---
+++ resources/views/components/frontend/partials/site-branding.blade.php
@@ -0,0 +1,27 @@
+
+<div class="site-branding-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="logo">
+                    <h1><a href="./"><img src="{{asset('ui/frontend/img/logo.png')}}"></a></h1>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="shopping-item">
+                    <a href="{{asset('ui/frontend/cart.html')}}">Cart - <span class="cart-amunt">$100</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">5</span></a>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                    <div class="text-center">
+                        <a href="./"><img class="img-fluid" src="{{asset('ui/frontend/img/me.jpg')}}"></a>
+                        <h2> Md Shahadat Kabir </h2>
+                        <h2>PHP with Laravel Framework</h2>
+                        <h2>Batch-12</h2>
+                        </div>
+                          </div>
+        </div>
+    </div>
+</div> <!-- End site branding area -->
